56106e5d099bacd0429b65dfc01706e7cdd72653
Nandesh Guru <nandesh@cloudcoreo.com>
2017-10-19 09:18:25 -0700

    Update config.rb

--- services/config.rb
+++ services/config.rb
@@ -65,7 +65,7 @@ coreo_agent_audit_profile 'linux-cis' do
   action :define
   selectors ['check-linux']
   profile 'https://github.com/coolguru/cis-dil-benchmark/archive/master.zip'
-  timeout 120
+  timeout 1
 end
 
 coreo_agent_rule_runner 'agent-rules' do
