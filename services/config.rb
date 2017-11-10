5fb24d5558d2cd45064f15f6be35d55b78035d4e
Nandesh Guru <nandesh@cloudcoreo.com>
2017-11-10 13:14:06 -0800

    Update config.rb

--- services/config.rb
+++ services/config.rb
@@ -62,54 +62,6 @@ coreo_agent_audit_rule 'cis-kubernetes-benchmark-1-1-2' do
   end
 end
 
-coreo_agent_audit_rule 'cis-kubernetes-benchmark-1-1-22' do
-  action :define
-  link 'http://kb.cloudcoreo.com/'
-  display_name 'Ensure that the --anonymous-auth argument is set to false'
-  description 'Disable anonymous requests to the API server.\n\nRationale: When enabled, requests that are not rejected by other configured authentication methods are treated as anonymous requests. These requests are then served by the API server. You should rely on authentication to authorize access and disallow anonymous requests.'
-  category 'Security'
-  suggested_action 'Disable anonymous requests to the API server.\n\nRationale: When enabled, requests that are not rejected by other configured authentication methods are treated as anonymous requests. These requests are then served by the API server. You should rely on authentication to authorize access and disallow anonymous requests.'
-  level 'high'
-  selectors ['check-kubectl']
-  timeout 120
-  control 'cis-kubernetes-benchmark-1.1.2' do
-    title 'Ensure that the --kubelet-certificate-authority argument is set as appropriate'
-    desc "Verify kubelet's certificate before establishing connection.\n\nRationale: The connections from the apiserver to the kubelet are used for fetching logs for pods, attaching (through kubectl) to running pods, and using the kubelet’s port-forwarding functionality. These connections terminate at the kubelet’s HTTPS endpoint. By default, the apiserver does not verify the kubelet’s serving certificate, which makes the connection subject to man-in-the-middle attacks, and unsafe to run over untrusted and/or public networks."
-    impact 1.0
-
-    tag cis: 'kubernetes:1.1.22'
-    tag level: 1
-
-    describe processes('kube-apiserver').commands.to_s do
-      it { should match(/--kubelet-certificate-authority=/) }
-    end
-  end
-end
-
-coreo_agent_audit_rule 'cis-kubernetes-benchmark-1-1-2' do
-  action :define
-  link 'http://kb.cloudcoreo.com/'
-  display_name 'Ensure that the --anonymous-auth argument is set to false'
-  description 'Disable anonymous requests to the API server.\n\nRationale: When enabled, requests that are not rejected by other configured authentication methods are treated as anonymous requests. These requests are then served by the API server. You should rely on authentication to authorize access and disallow anonymous requests.'
-  category 'Security'
-  suggested_action 'Disable anonymous requests to the API server.\n\nRationale: When enabled, requests that are not rejected by other configured authentication methods are treated as anonymous requests. These requests are then served by the API server. You should rely on authentication to authorize access and disallow anonymous requests.'
-  level 'high'
-  selectors ['check-kubectl']
-  timeout 120
-  control 'cis-kubernetes-benchmark-1.1.2' do
-    title 'Ensure that the --anonymous-auth argument is set to false'
-    desc "Disable anonymous requests to the API server.\n\nRationale: When enabled, requests that are not rejected by other configured authentication methods are treated as anonymous requests. These requests are then served by the API server. You should rely on authentication to authorize access and disallow anonymous requests."
-    impact 1.0
-
-    tag cis: 'kubernetes:1.1.2'
-    tag level: 1
-
-    describe processes('kube-apiserver').commands.to_s do
-      it { should_not match(/--insecure-allow-any-token/) }
-    end
-  end
-end
-
 coreo_agent_selector_rule "check-mongod" do
   action :define
   timeout 30
