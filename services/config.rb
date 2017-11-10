e67f01e2b687593fae202fb35dc8a3e70c8efad8
Nandesh Guru <nandesh@cloudcoreo.com>
2017-11-10 12:59:55 -0800

    Update config.rb

--- services/config.rb
+++ services/config.rb
@@ -28,6 +28,16 @@ coreo_agent_audit_rule 'echo-hello' do
   end
 end
 
+coreo_agent_selector_rule 'check-kubectl' do
+    action :define
+    timeout 30
+    control 'check-kubectl' do
+        describe command('kubectl') do
+            it { should exist }
+        end
+    end
+end
+
 coreo_agent_audit_rule 'cis-kubernetes-benchmark-1-1-2' do
   action :define
   link 'http://kb.cloudcoreo.com/'
@@ -36,7 +46,7 @@ coreo_agent_audit_rule 'cis-kubernetes-benchmark-1-1-2' do
   category 'Security'
   suggested_action 'Disable anonymous requests to the API server.\n\nRationale: When enabled, requests that are not rejected by other configured authentication methods are treated as anonymous requests. These requests are then served by the API server. You should rely on authentication to authorize access and disallow anonymous requests.'
   level 'low'
-  selectors ['check-echo']
+  selectors ['check-kubectl']
   timeout 120
   control 'cis-kubernetes-benchmark-1.1.2' do
     title 'Ensure that the --anonymous-auth argument is set to false'
@@ -48,6 +58,54 @@ coreo_agent_audit_rule 'cis-kubernetes-benchmark-1-1-2' do
 
     describe processes('kube-apiserver').commands.to_s do
       it { should match(/--anonymous-auth=false/) }
+    end
+  end
+end
+
+coreo_agent_audit_rule 'cis-kubernetes-benchmark-1-1-4' do
+  action :define
+  link 'http://kb.cloudcoreo.com/'
+  display_name 'Ensure that the --insecure-allow-any-token argument is not set'
+  description 'Do not allow any insecure tokens\n\nRationale: Accepting insecure tokens would allow any token without actually authenticating anything. User information is parsed from the token and connections are allowed.'
+  category 'Security'
+  suggested_action 'None'
+  level 'low'
+  selectors ['check-kubectl']
+  timeout 120
+  control 'cis-kubernetes-benchmark-1.1.4' do
+    title 'Ensure that the --insecure-allow-any-token argument is not set'
+    desc "Do not allow any insecure tokens\n\nRationale: Accepting insecure tokens would allow any token without actually authenticating anything. User information is parsed from the token and connections are allowed."
+    impact 1.0
+
+    tag cis: 'kubernetes:1.1.4'
+    tag level: 1
+
+    describe processes('kube-apiserver').commands.to_s do
+      it { should_not match(/--insecure-allow-any-token/) }
+    end
+  end
+end
+
+coreo_agent_audit_rule 'cis-kubernetes-benchmark-1-1-22' do
+  action :define
+  link 'http://kb.cloudcoreo.com/'
+  display_name 'Ensure that the --kubelet-certificate-authority argument is set as appropriate'
+  description "Verify kubelet's certificate before establishing connection.\n\nRationale: The connections from the apiserver to the kubelet are used for fetching logs for pods, attaching (through kubectl) to running pods, and using the kubelet’s port-forwarding functionality. These connections terminate at the kubelet’s HTTPS endpoint. By default, the apiserver does not verify the kubelet’s serving certificate, which makes the connection subject to man-in-the-middle attacks, and unsafe to run over untrusted and/or public networks."
+  category 'Security'
+  suggested_action 'Disable anonymous requests to the API server.\n\nRationale: When enabled, requests that are not rejected by other configured authentication methods are treated as anonymous requests. These requests are then served by the API server. You should rely on authentication to authorize access and disallow anonymous requests.'
+  level 'low'
+  selectors ['check-kubectl']
+  timeout 120
+  control 'cis-kubernetes-benchmark-1.1.22' do
+    title 'Ensure that the --kubelet-certificate-authority argument is set as appropriate'
+    desc "Verify kubelet's certificate before establishing connection.\n\nRationale: The connections from the apiserver to the kubelet are used for fetching logs for pods, attaching (through kubectl) to running pods, and using the kubelet’s port-forwarding functionality. These connections terminate at the kubelet’s HTTPS endpoint. By default, the apiserver does not verify the kubelet’s serving certificate, which makes the connection subject to man-in-the-middle attacks, and unsafe to run over untrusted and/or public networks."
+    impact 1.0
+
+    tag cis: 'kubernetes:1.1.22'
+    tag level: 1
+
+    describe processes('kube-apiserver').commands.to_s do
+      it { should match(/--kubelet-certificate-authority=/) }
     end
   end
 end
